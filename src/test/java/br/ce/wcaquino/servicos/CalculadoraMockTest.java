0455f04afa24127c524ef80c2ea2b3efd3eb544f
Daroz <geraldodarosfilho@hotmail.com>
2022-03-01 15:49:39 -0300

    Aula 34: Spy.

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,16 +1,37 @@
 package br.ce.wcaquino.servicos;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class CalculadoraMockTest {
+
+    @Mock
+    private Calculadora calcMock;
+
+    @Spy
+    private Calculadora calcSpy;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void devoMostrarDiferencaEntreMockSpy(){
+        Mockito.when(calcMock.somar(1, 2)).thenCallRealMethod();
+        Mockito.when(calcSpy.somar(1, 2)).thenReturn(8);
+
+        System.out.println(calcMock.somar(1, 2));
+    }
+
 
     @Test
     public void teste(){
         Calculadora calc = Mockito.mock(Calculadora.class);
         Mockito.when(calc.somar(Mockito.eq(1), Mockito.anyInt())).thenReturn(5);
     }
-
-
-
 }
